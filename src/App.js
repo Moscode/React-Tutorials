6b6a0bb2d57588b5a4a84f8d723deb63a121fb18
Moscode <odunoshomoses@gmail.com>
2022-03-06 22:59:53 +0000

    Counters for both users are working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Counter user='1'  className="App1"/>
+      <p>{
+          
+        }</p>
+      <Counter user='2' className="App2"/>
     </div>
   );
 }
